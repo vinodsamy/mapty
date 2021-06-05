c6419adae5a892831f31f85098ec0cc125eb377d
vino vinoth <vino929510@gmail.com>
2021-06-05 16:04:14 +0530

    [ok]render the workouts& move marker on click

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict"
 
 // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector(".form")
 const containerWorkouts = document.querySelector(".workouts")
@@ -62,6 +61,7 @@ const inputElevation = document.querySelector(".form__input--elevation")
 // })
 
 //with Class Version
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 class Workouts {
   date = new Date()
@@ -71,13 +71,18 @@ class Workouts {
     this.distance = distance
     this.duration = duration
   }
+  _setDescription() {
+    return (this.descriptionDate = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`)
+  }
 }
 
 class Running extends Workouts {
+  type = "running"
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration)
     this.cadence = cadence
     this.calcPace()
+    this._setDescription()
   }
 
   calcPace() {
@@ -88,10 +93,12 @@ class Running extends Workouts {
 }
 
 class Cycling extends Workouts {
+  type = "cycling"
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration)
     this.elevationGain = elevationGain
     this.calcSpeed()
+    this._setDescription()
   }
   calcSpeed() {
     this.speed = this.distance / (this.duration / 60)
@@ -113,6 +120,7 @@ class App {
     this._getPosition()
     form.addEventListener("submit", this._newWorkouts.bind(this))
     inputType.addEventListener("change", this._toggleElevationField)
+    containerWorkouts.addEventListener("click", this._moveToPopUp.bind(this))
   }
   _getPosition() {
     //here this_loadMap() is undefined because it's consider as regular function call
@@ -153,19 +161,6 @@ class App {
 
     const { lat, lng } = this.#mapEvent.latlng
     console.log(lat, lng)
-    L.marker([lat, lng])
-      .addTo(this.#map)
-      .bindPopup(
-        L.popup({
-          maxWidth: 300,
-          minWidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: "running-popup",
-        })
-      )
-      .setPopupContent("Workouts")
-      .openPopup()
 
     //check the validation
 
@@ -197,14 +192,99 @@ class App {
     console.log("workout", workout)
     //create new Object into workout array
     this.#workouts.push(workout)
+
+    this._renderWorkoutMarker(workout)
+    this._renderWorkout(workout)
     // console.log(this.#workout)
-    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = ""
+    this._hideForm()
+  }
+
+  _renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 300,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+      )
+      .setPopupContent(`${workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️"} ${workout.descriptionDate}`)
+      .openPopup()
+  }
+  _renderWorkout(workout) {
+    let html = `<li class="workout workout--${workout.type}" data-id="${workout.id}">
+          <h2 class="workout__title">${workout.descriptionDate}</h2>
+          <div class="workout__details">
+            <span class="workout__icon"> ${workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️"}</span>
+            <span class="workout__value">${workout.distance}</span>
+            <span class="workout__unit">km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⏱</span>
+            <span class="workout__value">${workout.duration}</span>
+            <span class="workout__unit">min</span>
+          </div>`
+    if (workout.type === "running") {
+      html += `
+           <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+        </li>
+      `
+    }
+    if (workout.type === "cycling") {
+      html += `
+        <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li>
+      `
+    }
+    form.insertAdjacentHTML("afterend", html)
   }
   _showForm(mapE) {
     console.log("thisKeyword", this)
     form.classList.remove("hidden")
     inputDistance.focus()
     this.#mapEvent = mapE
+  }
+
+  _hideForm() {
+    form.style.display = "none"
+    form.classList.add("hidden")
+    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = ""
+    setTimeout(() => (form.style.display = "grid"), 1000)
+  }
+  _moveToPopUp(e) {
+    const workoutEle = e.target.closest(".workout")
+    console.log(workoutEle)
+    console.log("workouts", this.#workouts)
+    if (!workoutEle) return
+    const selectedWorkout = this.#workouts.find((workout) => workout.id === workoutEle.dataset.id)
+    console.log(selectedWorkout)
+    this.#map.setView(selectedWorkout.coords, 13, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    })
   }
   _toggleElevationField() {
     inputElevation.closest(".form__row").classList.toggle("form__row--hidden")
